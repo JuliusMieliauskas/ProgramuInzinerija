f24e2e07753747d71e7d4f15d6d6781446ca452a
Julija Mikeliūnaitė <julija.mikeliunaite@mif.stud.vu.lt>
2024-10-15 14:09:27 +0300

    DRAFT fixed dotnet run errors

--- MyApp.Client/Pages/Memory.razor.cs
+++ MyApp.Client/Pages/Memory.razor.cs
@@ -2,51 +2,54 @@ using Microsoft.AspNetCore.Components;
 
 namespace MyApp.Client
 {
-    private List<Card> Cards = new();
-    private List<int> FlippedCards = new();
-
-    protected override void OnInitialized()
+    public class MemoryGame : ComponentBase
     {
-        var cardPairs = Enumerable.Range(1, 8).SelectMany(x => new[] { x, x }).ToList();
-        Random rng = new Random();
-        cardPairs = cardPairs.OrderBy(x => rng.Next()).ToList();
-        Cards = cardPairs.Select(x => new Card { Id = x, Number = x, Color = GetRandomColor() }).ToList();
-    }
-
-    private string GetRandomColor()
-    {
-        var colors = new[] { "red", "blue", "green", "yellow", "purple", "orange" };
-        return colors[Random.Shared.Next(colors.Length)];
-    }
-
-    private void FlipCard(int cardId)
-    {
-        if(FlippedCards.Contains(cardId))
+        public List<Card> Cards = new();
+        public List<int> FlippedCards = new();
+    
+        protected override void OnInitialized()
         {
-            return;
+            var cardPairs = Enumerable.Range(1, 8).SelectMany(x => new[] { x, x }).ToList();
+            Random rng = new Random();
+            cardPairs = cardPairs.OrderBy(x => rng.Next()).ToList();
+            Cards = cardPairs.Select(x => new Card { Id = x, Number = x, Color = GetRandomColor() }).ToList();
         }
-        FlippedCards.Add(cardId);
-        if(FlippedCards.Count == 2)
+    
+        private string GetRandomColor()
         {
-            if(Cards[FlippedCards[0]].Id == Cards[FlippedCards[1]].Id)
+            var colors = new[] { "red", "blue", "green", "yellow", "purple", "orange" };
+            return colors[Random.Shared.Next(colors.Length)];
+        }
+    
+        public void FlipCard(int cardId)
+        {
+            if(FlippedCards.Contains(cardId))
             {
-                FlippedCards.Clear();
+                return;
             }
-            else
+            FlippedCards.Add(cardId);
+            if(FlippedCards.Count == 2)
             {
-                Task.Delay(1000).ContinueWith(_ =>
+                if(Cards[FlippedCards[0]].Id == Cards[FlippedCards[1]].Id)
                 {
                     FlippedCards.Clear();
-                    StateHasChanged();
-                });
+                }
+                else
+                {
+                    Task.Delay(1000).ContinueWith(_ =>
+                    {
+                        FlippedCards.Clear();
+                        StateHasChanged();
+                    });
+                }
             }
+            StateHasChanged();
         }
-        StateHasChanged();
-    }
-    public class Card
-    {
-        public int Id { get; set; }
-        public int Number { get; set; }
-        public string Color {get; set;}
+        public class Card
+        {
+            public int Id { get; set; }
+            public int Number { get; set; }
+            public string Color {get; set;}
+        }
     }
 }
