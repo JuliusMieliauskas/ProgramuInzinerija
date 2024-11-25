923866b79eb2470486c25e481b895da619b34fbc
Julija Mikeliūnaitė <julija.mikeliunaite@mif.stud.vu.lt>
2024-11-25 19:18:56 +0200

    added some fields relating to difficulty and cards

--- Client/Pages/Memory/Memory.razor.cs
+++ Client/Pages/Memory/Memory.razor.cs
@@ -3,10 +3,20 @@ using Microsoft.AspNetCore.Components.Web;
 using System.Net.Http.Json;
 using System.Net.Http;
 using Shared;
+using MyApp.Pages.Calculatoor;
 
 namespace Client.Pages;
 
 public class MemoryBase : ComponentBase
 {
-    
+    public enum Difficulty { Easy, Medium, Hard }
+    Tuple<int, int> gridEasy = new  Tuple<int, int>(4, 4);
+    Tuple<int, int> gridMedium = new Tuple<int, int>(4, 6);
+    Tuple<int, int> gridHard = new Tuple<int, int>(6, 6);
+}
+
+public class Card
+{
+    private bool isFlippedUp = false;
+    private int cardFace = 0;
 }
