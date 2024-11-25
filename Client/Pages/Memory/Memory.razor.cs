4f76bac0dc33024855c3d150f6ae769e91522fc1
Julija Mikeliūnaitė <julija.mikeliunaite@mif.stud.vu.lt>
2024-11-25 18:31:34 +0200

    lost my progress, starting over, don't know what happened

---
+++ Client/Pages/Memory/Memory.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using System.Net.Http.Json;
+using System.Net.Http;
+using Shared;
+
+namespace Client.Pages;
+
+public class MemoryBase : ComponentBase
+{
+    
+}
