6515c9d1123e773bf1e01e2e47e5fb663cff020c
mik510 <mikeliunaite.j@gmail.com>
2024-12-01 19:58:54 +0200

    added initial fields for each difficulty

--- Client/Pages/Memory/EasyMem.razor.cs
+++ Client/Pages/Memory/EasyMem.razor.cs
@@ -7,5 +7,13 @@ using Shared;
 namespace Client.Pages;
 public class EasyMemBase : ComponentBase
 {
-    
+    private int gridHeight = 4;
+    int gridWidth = 4;
+    int pairs = 8;
+}
+
+public class EasyCard
+{
+    bool isFlipped = false;
+    enum cardFaces {E1=1, E2, E3, E4, E5, E6, E7, E8}
 }
