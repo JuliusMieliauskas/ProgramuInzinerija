901dba329c33edd7e61a9d68e211911731660bc8
mik510 <mikeliunaite.j@gmail.com>
2024-12-06 14:49:30 +0200

    minor changes

--- Client/Pages/Memory/EasyMem.razor.cs
+++ Client/Pages/Memory/EasyMem.razor.cs
@@ -9,11 +9,12 @@ public class EasyMemBase : ComponentBase
 {
     private int gridHeight = 4;
     int gridWidth = 4;
-    int pairs = 8;
+    int timer = 0;
 }
 
 public class EasyCard
 {
-    bool isFlipped = false;
+    int flippedCards = 0;
+    int pairs = 8;
     enum cardFaces {E1=1, E2, E3, E4, E5, E6, E7, E8}
 }
