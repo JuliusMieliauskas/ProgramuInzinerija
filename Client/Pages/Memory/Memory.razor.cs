3fad0a5db2237856cfe650e87eb4074f083b12ad
mik510 <mikeliunaite.j@gmail.com>
2024-12-06 14:23:35 +0200

    removed cards class, as its not needed here

--- Client/Pages/Memory/Memory.razor.cs
+++ Client/Pages/Memory/Memory.razor.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Components.Web;
 using System.Net.Http.Json;
 using System.Net.Http;
 using Shared;
-using MyApp.Pages.Calculatoor;
 
 namespace Client.Pages;
 
@@ -16,10 +15,4 @@ public class MemoryBase : ComponentBase
     protected void NavigateToEasy() => NavigationManager.NavigateTo("/easyMem");
     protected void NavigateToMedium() => NavigationManager.NavigateTo("/mediumMem");
     protected void NavigateToHard() => NavigationManager.NavigateTo("/hardMem");
-}
-
-public class Card
-{
-    private bool isFlippedUp = false;
-    private int cardFace = 0;
 }
