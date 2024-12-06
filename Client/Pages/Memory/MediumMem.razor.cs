056c95578edad34eab5dc6034e4c35bfd1ef0aab
mik510 <mikeliunaite.j@gmail.com>
2024-12-06 13:47:57 +0200

    fixed error, too many card faces in medium level

--- Client/Pages/Memory/MediumMem.razor.cs
+++ Client/Pages/Memory/MediumMem.razor.cs
@@ -15,5 +15,6 @@ public class MediumMemBase : ComponentBase
 public class MediumCard
 {
     bool isFlipped = false;
-    enum cardFaces {M1=1, M2, M3, M4, M5, M6, M7, M8, M9, M10, M11, M12, M13, M14, M15, M16}
+    enum cardFaces {M1=1, M2, M3, M4, M5, M6, M7, M8, 
+                    M9, M10, M11, M12}
 }
