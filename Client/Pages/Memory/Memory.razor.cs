0e70b1a1459159a14d18b9d5534cf531ab37fe00
mik510 <mikeliunaite.j@gmail.com>
2024-12-17 13:18:39 +0200

    minor changes

--- Client/Pages/Memory/Memory.razor.cs
+++ Client/Pages/Memory/Memory.razor.cs
@@ -8,9 +8,7 @@ namespace Client.Pages;
 public class MemoryBase : ComponentBase
 {
     protected int missmatches = 0;
-    protected string message;
     protected bool gameOver = false;
-    private object MemoryResultList;
 
     public enum Difficulty { Easy, Medium, Hard }
     [Inject]
@@ -21,23 +19,4 @@ public class MemoryBase : ComponentBase
     protected void NavigateToHard() => NavigationManager.NavigateTo("/hardMem");
 
     protected HttpClient? _httpClient { get; set; }
-    //protected List<MemoryGameResult> memoryResultList = new List<MemeoryGameResult>();
-
-    protected async Task SaveResults(){
-        try
-        {
-            if (_httpClient == null) throw new ClientNullException();
-        }
-        catch(ClientNullException){
-            message = "Server Error, results cannot be saved!";
-            return;
-        }
-        if (gameOver){
-            MemoryGameResult result = new MemoryGameResult(missmatches: missmatches);
-            //MemoryResultList.Add(result);
-
-            missmatches = 0;
-            await _httpClient.PostAsJsonAsync("api/memorygameresults", result);
-        }
-    }
 }
